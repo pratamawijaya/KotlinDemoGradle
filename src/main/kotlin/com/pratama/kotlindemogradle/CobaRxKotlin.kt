6d5657b5c039c314be67126dae66d66c9b413125
pratamawijaya <set.mnemonix@gmail.com>
2017-07-13 16:55:37 +0700

    init code

---
+++ src/main/kotlin/com/pratama/kotlindemogradle/CobaRxKotlin.kt
@@ -0,0 +1,31 @@
+package com.pratama.kotlindemogradle
+
+import io.reactivex.Observable
+
+/**
+ * Created by pratama on 7/13/17.
+ */
+
+fun main(args: Array<String>) {
+    val list = listOf("Alpha", "Beta", "Gamma", "Delta", "Epsilon")
+
+    // RxKotlin
+//    list.toObservable()
+//        .filter { it.length >= 5 }
+//        .subscribeBy(
+//            onNext = { println(it) },
+//            onError = { it.printStackTrace() },
+//            onComplete = { println("\nDone") }
+//        )
+
+    // RxJava
+    Observable.fromIterable(list)
+        .filter { it.length >= 5 }
+        .subscribe({
+            result ->
+            println(result)
+        }, {
+            error ->
+            println("error $error")
+        })
+}
---
+++ src/main/kotlin/com/pratama/kotlindemogradle/Hello.kt
@@ -0,0 +1,12 @@
+package com.pratama.kotlindemogradle
+
+fun main(args: Array<String>) {
+
+    var name: String? = null
+
+    println("$name")
+
+    val nameLen = name?.length
+
+    println("panjang $nameLen")
+}
