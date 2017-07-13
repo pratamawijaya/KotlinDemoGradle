bd8d99ed4aa1ccd9d44ff09d00e153877cbda72f
pratamawijaya <set.mnemonix@gmail.com>
2017-07-13 17:00:30 +0700

    activate rxkotlin

--- src/main/kotlin/com/pratama/kotlindemogradle/CobaRxKotlin.kt
+++ src/main/kotlin/com/pratama/kotlindemogradle/CobaRxKotlin.kt
@@ -1,6 +1,8 @@
 package com.pratama.kotlindemogradle
 
 import io.reactivex.Observable
+import io.reactivex.rxkotlin.subscribeBy
+import io.reactivex.rxkotlin.toObservable
 
 /**
  * Created by pratama on 7/13/17.
@@ -10,13 +12,13 @@ fun main(args: Array<String>) {
     val list = listOf("Alpha", "Beta", "Gamma", "Delta", "Epsilon")
 
     // RxKotlin
-//    list.toObservable()
-//        .filter { it.length >= 5 }
-//        .subscribeBy(
-//            onNext = { println(it) },
-//            onError = { it.printStackTrace() },
-//            onComplete = { println("\nDone") }
-//        )
+    list.toObservable()
+        .filter { it.length >= 5 }
+        .subscribeBy(
+            onNext = { println(it) },
+            onError = { it.printStackTrace() },
+            onComplete = { println("\nDone") }
+        )
 
     // RxJava
     Observable.fromIterable(list)
